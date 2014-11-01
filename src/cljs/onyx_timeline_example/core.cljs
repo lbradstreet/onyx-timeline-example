bd14402055adf57680e07b997fa49c44c732a86f
Lucas Bradstreet <lucasbradstreet@gmail.com>
2014-11-01 18:24:34 +0800

    Put timeline component into grid along with a new word count component. Match the new message types for each of these and put them on their correct channels.

--- src/cljs/onyx_timeline_example/core.cljs
+++ src/cljs/onyx_timeline_example/core.cljs
@@ -3,16 +3,21 @@
                    [cljs.core.async.macros :refer [go-loop go alt!]])
   (:require [om.core :as om :include-macros true]
             [om-bootstrap.panel :as p]
+            [om-bootstrap.table :refer  [table]]
+            [om-bootstrap.grid :as g]
             [om-tools.dom :as d :include-macros true]
             [om-tools.core :refer-macros [defcomponent]]
             [taoensso.sente  :as sente  :refer (cb-success?)]
             [taoensso.sente.packers.transit :as sente-transit]
             [cljs.core.async :as async :refer [<! >! chan put! alts! timeout]]))
 
-(def timeline-chan (chan 10000))
+; Put channels in root shared rather than refer to def
+(def timeline-chan (chan))
+(def agg-chan (chan))
 
-(defonce app-state (atom {:latest-id 0
-                          :items []}))
+(defonce app-state (atom {:top-word-counts {}
+                          :timeline {:latest-id 0
+                                     :items []}}))
 
 (def packer
   "Defines our packing (serialization) format for client<->server comms."
@@ -30,19 +35,47 @@
          [:chsk/recv payload] (let [[msg-type msg] payload]
                                 (println "Payload " payload)
                                 (match [msg-type msg]
-                                       [:tweet/new tweet] (put! timeline-chan tweet)))
+                                       [:tweet/new tweet-text] (put! timeline-chan tweet-text)
+                                       [:agg/top-word-count counts] (put! agg-chan counts)))
          :else (print "Unmatched event: %s" event)))
 
 (defonce chsk-router (sente/start-chsk-router! ch-chsk event-handler))
 
-(defn add-item [state item]
-  (-> state
-      (update-in [:items] (fn [items] 
-                            (let [trunc-items (take 100 items)]
-                              (cons {:id (:latest-id state)
-                                     :text (:word item)} 
-                                    trunc-items))))
+(defn add-tweet [timeline tweet]
+  (-> timeline
+      (update-in [:items] (fn [tweets] 
+                            (let [trunc-tweets (take 100 tweets)]
+                              (cons {:id (:latest-id timeline)
+                                     :tweet tweet} 
+                                    trunc-tweets))))
       (update-in [:latest-id] inc)))
+
+
+(defcomponent top-word-counts [data owner]
+  (will-mount [_]
+              (go-loop [] 
+                       ; Use alt for now, may have some other channels here in the future
+                       (alt!
+                         agg-chan
+                         ([msg]
+                          (do (println (str "Received event on timeline channel: " msg))
+                              (om/update! data msg))))
+                       (recur)))
+  (render-state [_ _]
+                (p/panel
+                  {:header "Word counts"
+                   :list-group 
+                   (table {:striped? true :bordered? true :condensed? true :hover? true}
+                          (d/thead
+                            (d/tr
+                              (d/th "Count")
+                              (d/th "Word")))
+                          (d/tbody
+                            (for [word-count (reverse (sort-by val data))]
+                              (d/tr {:key (key word-count)}
+                                    (d/td (val word-count))
+                                    (d/td (key word-count))))))}
+                  nil)))
 
 (defcomponent timeline [data owner]
   (will-mount [_]
@@ -52,20 +85,31 @@
                          timeline-chan
                          ([msg]
                           (do (println (str "Received event on timeline channel: " msg))
-                              (om/transact! data #(add-item % msg)))))
+                              (om/transact! data #(add-tweet % msg)))))
                        (recur)))
   (render-state [_ _]
                 (p/panel
-                  {:header "Results panel"
+                  {:header "Timeline"
                    :list-group (d/ul {:class "list-group"}
                                      (for [item (:items data)]
                                        (d/li {:key (:id item)
                                               :class "list-group-item"
-                                              :style {:color "blue"}}
-                                             (:text item))))}
+                                              :style {}}
+                                             (:tweet item))))}
                   nil)))
 
+(defcomponent app [data owner]
+  (render-state [_ _]
+                (d/div
+                  {:class "grids-examples"}
+                  (g/grid {}
+                          (g/row {:class "show-grid"}
+                                 (g/col {:xs 12 :md 8}
+                                        (om/build timeline (:timeline data) {}))
+                                 (g/col {:xs 6 :md 4}
+                                        (om/build top-word-counts (:top-word-counts data) {})))))))
+
 (defn main []
-  (om/root timeline 
+  (om/root app 
            app-state 
            {:target (. js/document (getElementById "app"))}))
