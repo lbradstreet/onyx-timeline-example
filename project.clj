e191781eb2cbc89a5e8ce0b383c216f5b20eec56
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-02-14 13:55:14 +0800

    Bump deps.

--- project.clj
+++ project.clj
@@ -10,36 +10,37 @@
   :test-paths ["test/clj"]
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2371" :scope "provided"]
+                 [prismatic/schema "0.3.7"]
+                 [org.clojure/clojurescript "0.0-2850"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [org.clojure/core.match "0.2.2"]
-                 [org.clojure/tools.logging  "0.3.0"]
-                 [ring/ring-defaults  "0.1.1"]
+                 [org.clojure/tools.logging  "0.3.1"]
+                 [ring/ring-defaults  "0.1.4"]
                  [com.cognitect/transit-clj "0.8.259"]
-                 [com.cognitect/transit-cljs "0.8.188"]
+                 [com.cognitect/transit-cljs "0.8.205"]
                  [com.stuartsierra/component "0.2.2"]
-                 [com.twitter/hbc-core "2.0.0"
+                 [com.twitter/hbc-core "2.2.0"
                   :exclusions [commons-codec
                                com.google.guava/guava
                                org.apache.httpcomponents/httpclient]]
-                 [com.mdrogalis/onyx "0.4.1-SNAPSHOT"]
-                 [com.mdrogalis/onyx-core-async "0.4.0"]
-                 [com.mdrogalis/lib-onyx "0.4.0"]
-                 [com.taoensso/sente "1.2.0"]
-                 [cheshire "5.3.1"]
-                 [ring "1.3.1"]
-                 [compojure "1.2.0"]
+                 [potemkin "0.3.11"]
+                 [com.mdrogalis/onyx "0.5.1"]
+                 [com.mdrogalis/onyx-core-async "0.5.0"]
+                 [com.mdrogalis/lib-onyx "0.5.0"]
+                 [com.taoensso/sente "1.3.0"]
+                 [cheshire "5.4.0"]
+                 [ring "1.3.2"]
+                 [compojure "1.3.1"]
                  [enlive "1.1.5"]
-                 [om "0.7.3"]
-                 [com.facebook/react "0.11.2"]
-                 [figwheel "0.1.4-SNAPSHOT"]
+                 [org.omcljs/om "0.8.8"]
+                 [figwheel "0.2.3-SNAPSHOT"]
                  [environ "1.0.0"]
-                 [com.cemerick/piggieback "0.1.3"]
-                 [weasel "0.4.2"]
-                 [leiningen "2.5.0"]
+                 [com.cemerick/piggieback "0.1.5"]
+                 [weasel "0.5.0"]
+                 [leiningen "2.5.1"]
                  [http-kit "2.1.19"]
-                 [racehub/om-bootstrap  "0.3.1"]
-                 [prismatic/om-tools "0.3.3"]]
+                 [racehub/om-bootstrap "0.4.0" :exclusions [om]]
+                 [prismatic/om-tools "0.3.10" :exclusions [om]]]
 
   :plugins [[lein-cljsbuild "1.0.3"]
             [lein-environ "1.0.0"]]
@@ -70,7 +71,7 @@
   :profiles {:dev {:repl-options {:init-ns onyx-timeline-example.server
                                   :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
                    :hooks [leiningen.cljsbuild]
-                   :plugins [[lein-figwheel "0.1.4-SNAPSHOT"]]
+                   :plugins [[lein-figwheel "0.2.3-SNAPSHOT"]]
                    :figwheel {:http-server-root "public"
                               :port 3449
                               :css-dirs ["resources/public/css"]}
